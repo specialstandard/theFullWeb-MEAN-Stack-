a7ce1059f7c7c8a6e38eee3ad1ec8bdcd0f7a610
Brian Ardito <specialstandard@users.noreply.github.com>
2016-02-26 13:16:48 +1100

    Added files via upload
    Fixed bug with crypto.pbkdf2Sync()
    In UserSchema.methods.setPassword()
    and
    UserSchema.methods.validPassword()

--- models/Users.js
+++ models/Users.js
@@ -9,10 +9,10 @@ var UserSchema = new mongoose.Schema({
 });
 UserSchema.methods.setPassword = function(password){
   this.salt = crypto.randomBytes(16).toString('hex');
-  this.hash = crypto.pbkdf2sync(password, this.salt, 1000, 64).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 };
 UserSchema.methods.validPassword = function(password){
-  var hash = crypto.pbkdf2sync(password, this.salt, 1000, 64).toString('hex');
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
   return this.hash === hash;
 };
 UserSchema.methods.generateJWT = function(){
